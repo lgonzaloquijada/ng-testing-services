2be01e7c531f2394ea71c26330ca743568a50303
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-03 18:09:36 -0700

    feat: add products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from '@components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+];
