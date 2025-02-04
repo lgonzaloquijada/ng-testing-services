6956f3c7630635124336404a17f23b60f8f4edf0
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-03 19:25:17 -0700

    feat: add product service and list

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { Product } from '@models/product.model';
+import { ProductsService } from '@services/product.service';
 
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  private productService = inject(ProductsService);
+  products: WritableSignal<Product[]> = signal([]);
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productService.getAllSimple().subscribe((products) => {
+      this.products.set(products);
+    });
+  }
 }
