b5bbaae027603338bd4192304d4bf337b1146bbd
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-04 18:45:08 -0700

    test: adds test for create.

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -6,7 +6,7 @@ import {
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { Product } from '@models/product.model';
+import { CreateProductDTO, Product } from '@models/product.model';
 import { environment } from '@src/environments/environment';
 import { ProductMock } from '@models/product.mock';
 
@@ -186,6 +186,34 @@ fdescribe('ProductService', () => {
       expect(req.request.params.get('limit')).toEqual(limit.toString());
       expect(req.request.params.get('offset')).toEqual(offset.toString());
       req.flush(mockProducts);
+    });
+  });
+
+  describe('Tests for create', () => {
+    it('should return the created product', (doneFn) => {
+      // Arrange
+      const mockProduct: Product = ProductMock.getOne();
+      const dto: CreateProductDTO = {
+        title: 'Product 1',
+        price: 100,
+        images: ['image1.jpg', 'image2.jpg'],
+        description: 'Description',
+        categoryId: 12,
+      };
+
+      productService.create({ ...dto }).subscribe((product) => {
+        // Assert
+        expect(product).toEqual(mockProduct);
+        doneFn();
+      });
+
+      // Act
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toBe('POST');
+      req.flush(mockProduct);
+      expect(req.request.body).toEqual(dto);
+      httpController.verify();
     });
   });
 });
