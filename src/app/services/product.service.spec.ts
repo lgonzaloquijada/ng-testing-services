9f1abcb061222b782972bc4a7964dd0ba80b877a
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-04 17:30:00 -0700

    test: add tests for getAll, getAll with taxes and getAll with taxes and 3 tries

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -44,4 +44,100 @@ fdescribe('ProductService', () => {
       req.flush(mockProducts);
     });
   });
+
+  describe('Tests for getAll', () => {
+    it('should return an array of products', (doneFn) => {
+      // Arrange
+      const mockProducts: Product[] = ProductMock.get(3);
+
+      productService.getAll().subscribe((products) => {
+        // Assert
+        expect(products.length).toEqual(mockProducts.length);
+        doneFn();
+      });
+
+      // Act
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockProducts);
+    });
+
+    it('should return an array of products with taxes', (doneFn) => {
+      // Arrange
+      const mockProducts: Product[] = [
+        {
+          ...ProductMock.getOne(),
+          price: 100,
+        },
+        {
+          ...ProductMock.getOne(),
+          price: 200,
+        },
+        {
+          ...ProductMock.getOne(),
+          price: 300,
+        },
+      ];
+
+      productService.getAll().subscribe((products) => {
+        // Assert
+        expect(products[0].taxes).toEqual(100 * 0.19);
+        expect(products[1].taxes).toEqual(200 * 0.19);
+        expect(products[2].taxes).toEqual(300 * 0.19);
+        doneFn();
+      });
+
+      // Act
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockProducts);
+    });
+
+    it('should return an array of products with taxes and retry 3 times', (doneFn) => {
+      // Arrange
+      const mockProducts: Product[] = ProductMock.get(3);
+
+      productService.getAll().subscribe((products) => {
+        // Assert
+        expect(products).toEqual(
+          mockProducts.map((item) => {
+            return {
+              ...item,
+              taxes: 0.19 * item.price,
+            };
+          })
+        );
+        doneFn();
+      });
+
+      // Act
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockProducts, {
+        status: 500,
+        statusText: 'Internal Server Error',
+      });
+
+      const req2 = httpController.expectOne(url);
+      expect(req2.request.method).toBe('GET');
+      req2.flush(mockProducts, {
+        status: 500,
+        statusText: 'Internal Server Error',
+      });
+
+      const req3 = httpController.expectOne(url);
+      expect(req3.request.method).toBe('GET');
+      req3.flush(
+        mockProducts.map((item) => {
+          return {
+            ...item,
+            taxes: 0.19 * item.price,
+          };
+        })
+      );
+    });
+  });
 });
